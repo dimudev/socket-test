71e8e9dff182d24cf0f93caf6fb569edd18e1fe8
diego <dimudeveloper@gmail.com>
2025-05-12 15:53:58 -0500

    add despliegue de index.html

--- models/server.js
+++ models/server.js
@@ -1,6 +1,5 @@
-// Servidor de Express
 const express = require('express');
-const http =require('http')
+const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
@@ -8,12 +7,12 @@ const Sockets = require('./sockets');
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
 
     // http server
     this.server = http.createServer(this.app);
     
-    // Configuración de socket server
+    // Configuración de socket server
     this.io = socketIo(this.server);
   }
 
@@ -21,24 +20,25 @@ class Server {
     new Sockets(this.io);
   }
 
-
   middlewares() {
-    // Desplegar el directorio público
+    // Desplegar el directorio público
     this.app.use(express.static(path.resolve(__dirname, '../public')));
 
+    // Ruta para servir el archivo index.html
+    this.app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+    });
   }
 
-  execute() {    
-  this.middlewares();
-  // Inizializar mis sockets
-  this.configurarSockets();
+  execute() {
+    this.middlewares();
+    // Inicializar mis sockets
+    this.configurarSockets();
 
-  this.server.listen(this.port, () => {
-    console.log('Servidor corriendo en puerto:', this.port);
-  });
+    this.server.listen(this.port, () => {
+      console.log('Servidor corriendo en puerto:', this.port);
+    });
   }
-
-
 }
 
 module.exports = Server;
